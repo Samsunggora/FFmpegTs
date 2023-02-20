7bbb96e4b2995010d2500cecbb2180f1e40d267a
Egor LIkhacev <samsunggora@gmail.com>
2023-02-21 00:38:32 +0100

    Input processing for the application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
+import { PromptService } from './core/prompt/prompt.service';
+
 export class App {
-    run() {
-        console.log("run")
+    async run() {
+        const res = await (new PromptService()).input<number>('Число', 'number');
+        console.log(res);
     }
 }
-
-const app = new App();
-app.run()
